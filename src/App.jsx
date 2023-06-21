14b8e09aa4a7d9f76e6aaca954c64d873a63af82
GOKULDAS V K <gokuldasvk14218@gmail.com>
2023-06-21 11:52:14 +0530

    s commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/Register" exact element={<Register />}></Route>
+      </Routes>
+
+    </>
+  )
+}
+
+
+export default App
